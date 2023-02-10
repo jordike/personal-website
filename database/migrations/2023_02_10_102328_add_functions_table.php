bba4bf0df9e1e619ae413ad94d35dc6d20e27c5d
Jordi Keijzers <jordikeijzers@gmail.com>
2023-02-10 11:32:10 +0100

    Database migrations

---
+++ database/migrations/2023_02_10_102328_add_functions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("experience_functions", function(Blueprint $table) {
+            $table->id();
+            $table->foreignId("experience_id")->constrained("experience");
+            $table->string("function_title");
+            $table->string("description")->nullable();
+            $table->date("start_date");
+            $table->date("end_date")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("experience_functions");
+    }
+};
