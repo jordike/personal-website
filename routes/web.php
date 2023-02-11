4860dd508cc7bc82f8df933206a02e8310a7157c
Jordi Keijzers <jordikeijzers@gmail.com>
2023-02-11 14:47:18 +0100

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ExperienceController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\CvController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view("/", "index")->name("home");
+Route::resource("/projects", ProjectsController::class)->names("projects");
+Route::resource("/experience", ExperienceController::class)->names("experience");
+Route::get("/curriculum-vitae", [CvController::class, "index"])->name("cv");
+Route::get("/contact", [ContactController::class, "index"])->name("contact");
+
+Route::controller(AuthController::class)->group(function() {
+    Route::middleware("guest")->group(function() {
+        Route::view("/login", "login")->name("login");
+        Route::post("/login", "login")->name("login.post");
+    });
+    Route::middleware("auth")->group(function() {
+        Route::get("/account/profile", "profile")->name("profile");
+        Route::get("/logout", "logout")->name("logout");
+    });
+});
+
+Route::controller(AdminController::class)->prefix("/admin")->group(function() {
+    Route::get("/", "index")->name("admin");
+    Route::get("create-account", "index")->name("admin.create-account");
+    Route::post("create-account", "create_account")->name("admin.create-account.post");
 });
