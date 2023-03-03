e40dca92a97ef24e4972fb827609f7171cdd36dc
Jordi Keijzers <jordikeijzers@gmail.com>
2023-03-03 11:30:04 +0100

    Save current selected theme

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,10 +1,12 @@
 ﻿const bodyElement = document.getElementsByTagName("body").item(0);
 const toggleButtonIcon = document.getElementById("theme-toggle-icon");
 
-function toggleTheme() {
-    bodyElement.classList.toggle("dark-theme");
+function darkThemeOn() {
+    return localStorage.getItem("dark-theme") == "true";
+}
 
-    if (bodyElement.classList.contains("dark-theme")) {
+function updateTogglerIcon() {
+    if (darkThemeOn()) {
         toggleButtonIcon.classList.remove("fa-moon");
         toggleButtonIcon.classList.add("fa-sun");
     } else {
@@ -12,3 +14,33 @@ function toggleTheme() {
         toggleButtonIcon.classList.add("fa-moon");
     }
 }
+
+function setTheme(dark) {
+    if (dark) {
+        bodyElement.classList.add("dark-theme");
+    } else {
+        bodyElement.classList.remove("dark-theme");
+    }
+
+    localStorage.setItem("dark-theme", dark);
+
+    updateTogglerIcon();
+}
+
+function loadTheme() {
+    const dark = localStorage.getItem("dark-theme");
+
+    if (dark !== null) {
+        setTheme(dark == "true");
+    }
+}
+
+function toggleTheme() {
+    bodyElement.classList.toggle("dark-theme");
+
+    localStorage.setItem("dark-theme", !darkThemeOn());
+
+    updateTogglerIcon();
+}
+
+loadTheme();
