faab5727cc08335f1eca27b381d075035ff55fe2
Jordi Keijzers <jordikeijzers@gmail.com>
2023-03-03 11:28:22 +0100

    Add page for error code 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.main")
+
+@section("title", "Internal server error")
+
+@section("content")
+    <div class="container">
+        <h1>500 - Internal Server Error</h1>
+    </div>
+@endsection
