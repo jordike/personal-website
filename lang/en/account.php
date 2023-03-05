5b8bbaf1f0af5cb0e5f8461fd56baed26b204c81
Jordi Keijzers <jordikeijzers@gmail.com>
2023-03-05 14:30:55 +0100

    Added translation for missing translation key

---
+++ lang/en/account.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "email_or_password_incorrect" => "You have entered either the wrong email or password. Make sure you have entered the correct information, and try again."
+];
---
+++ lang/nl/account.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "email_or_password_incorrect" => "U heeft of het verkeerde e-mailadres ingevuld, of het verkeerde wachtwoord. Controleer of u de juiste gegevens hebt ingevuld, en probeer het opnieuw."
+];
